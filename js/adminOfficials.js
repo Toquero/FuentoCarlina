211908bc25923f5833d0c78884fb0c02a4e4cbd3
Christy TOQUERO <christy.toquero@datawords.com>
2023-08-20 19:47:08 +0800

    nayn

---
+++ js/adminOfficials.js
@@ -0,0 +1,30 @@
+// Get references to the buttons
+const viewMoreBtn1 = document.getElementById('viewMoreBtn1');
+const viewMoreBtn2 = document.getElementById('viewMoreBtn2');
+const viewMoreBtn3 = document.getElementById('viewMoreBtn3');
+const viewMoreBtn4 = document.getElementById('viewMoreBtn4');
+
+// Add click event listeners to the buttons
+viewMoreBtn1.addEventListener('click', () => {
+  // Code to execute when the first button is clicked
+  // For example, you can navigate to a new page or show more details
+  alert('View More clicked for Card 1');
+});
+
+viewMoreBtn2.addEventListener('click', () => {
+  // Code to execute when the second button is clicked
+  // For example, you can navigate to a new page or show more details
+  alert('View More clicked for Card 2');
+});
+
+viewMoreBtn3.addEventListener('click', () => {
+  // Code to execute when the third button is clicked
+  // For example, you can navigate to a new page or show more details
+  alert('View More clicked for Card 3');
+});
+
+viewMoreBtn4.addEventListener('click', () => {
+  // Code to execute when the fourth button is clicked
+  // For example, you can navigate to a new page or show more details
+  alert('View More clicked for Card 4');
+});
